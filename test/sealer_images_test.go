4cbc81e4d72577b97925acb8b14491ade3058a74
wb-hjh1748756566 <wb-hjh933779@alibaba-inc.com>
2021-05-29 19:01:04 +0800

    add image e2e test

--- test/sealer_images_test.go
+++ test/sealer_images_test.go
@@ -39,10 +39,10 @@ var _ = Describe("sealer image", func() {
 			"sealer-io/kubernetes:v1.19.9",
 			"kubernetes:v1.19.9",
 		}
-		for _, imageName := range pullImageNames {
-			It(fmt.Sprintf("pull image %s", imageName), func() {
-				image.DoImageOps("pull", imageName)
-				image.DoImageOps("rmi", imageName)
+		for i := range pullImageNames {
+			It(fmt.Sprintf("pull image %s", pullImageNames[i]), func() {
+				image.DoImageOps("pull", pullImageNames[i])
+				image.DoImageOps("rmi", pullImageNames[i])
 			})
 		}
 
@@ -51,9 +51,9 @@ var _ = Describe("sealer image", func() {
 			"registry.cn-qingdao.aliyuncs.com:latest",
 			"sealer-io:latest",
 		}
-		for _, imageName := range faultPullImageNames {
-			It(fmt.Sprintf("pull fault image %s", imageName), func() {
-				sess, err := testhelper.Start(fmt.Sprintf("%s pull %s", settings.SealerBinPath, imageName))
+		for i := range faultPullImageNames {
+			It(fmt.Sprintf("pull fault image %s", faultPullImageNames[i]), func() {
+				sess, err := testhelper.Start(fmt.Sprintf("%s pull %s", settings.SealerBinPath, faultPullImageNames[i]))
 				Expect(err).NotTo(HaveOccurred())
 				Eventually(sess, settings.MaxWaiteTime).ShouldNot(Exit(0))
 			})
@@ -61,11 +61,11 @@ var _ = Describe("sealer image", func() {
 	})
 
 	Context("remove image", func() {
-		It(fmt.Sprintf("remove image %s", settings.TestImageName), func() {
+		It(fmt.Sprintf("remove image %s", settings.ImageNameForRun), func() {
 			beforeDirMd5, err := utils.DirMD5(filepath.Dir(common.DefaultImageRootDir))
 			Expect(err).NotTo(HaveOccurred())
-			image.DoImageOps("pull", settings.TestImageName)
-			image.DoImageOps("rmi", settings.TestImageName)
+			image.DoImageOps("pull", settings.ImageNameForRun)
+			image.DoImageOps("rmi", settings.ImageNameForRun)
 			afterDirMd5, err := utils.DirMD5(filepath.Dir(common.DefaultImageRootDir))
 			Expect(err).NotTo(HaveOccurred())
 			Expect(afterDirMd5).To(Equal(beforeDirMd5))
@@ -73,39 +73,39 @@ var _ = Describe("sealer image", func() {
 
 		It("remove tag image", func() {
 			tagImageName := "e2e_image_test:v0.01"
-			image.DoImageOps("pull", settings.TestImageName)
+			image.DoImageOps("pull", settings.ImageNameForRun)
 			beforeDirMd5, err := utils.DirMD5(filepath.Dir(common.DefaultImageRootDir))
 			Expect(err).NotTo(HaveOccurred())
-			image.TagImages(settings.TestImageName, tagImageName)
+			image.TagImages(settings.ImageNameForRun, tagImageName)
 
 			image.DoImageOps("rmi", tagImageName)
 			afterDirMd5, err := utils.DirMD5(filepath.Dir(common.DefaultImageRootDir))
 			Expect(err).NotTo(HaveOccurred())
 			Expect(afterDirMd5).To(Equal(beforeDirMd5))
 
-			image.DoImageOps("rmi", settings.TestImageName)
+			image.DoImageOps("rmi", settings.ImageNameForRun)
 		})
 	})
 
 	Context("push image", func() {
 		BeforeEach(func() {
 			registry.Login()
-			image.DoImageOps("pull", settings.TestImageName)
+			image.DoImageOps("pull", settings.ImageNameForRun)
 		})
 		AfterEach(func() {
 			registry.Logout()
-			image.DoImageOps("rmi", settings.TestImageName)
+			image.DoImageOps("rmi", settings.ImageNameForRun)
 		})
 		var pushImageNames = []string{
 			"registry.cn-qingdao.aliyuncs.com/sealer-io/e2e_image_test:v0.01",
 			"sealer-io/e2e_image_test:v0.01",
 			"e2e_image_test:v0.01",
 		}
-		for _, pushImageName := range pushImageNames {
-			It(fmt.Sprintf("push image %s", pushImageName), func() {
-				image.TagImages(settings.TestImageName, pushImageName)
-				image.DoImageOps("push", pushImageName)
-				image.DoImageOps("rmi", pushImageName)
+		for i := range pushImageNames {
+			It(fmt.Sprintf("push image %s", pushImageNames[i]), func() {
+				image.TagImages(settings.ImageNameForRun, pushImageNames[i])
+				image.DoImageOps("push", pushImageNames[i])
+				image.DoImageOps("rmi", pushImageNames[i])
 			})
 		}
 	})
