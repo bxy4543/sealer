8372dd307e15cbf8dd14277d3becf12272a8374d
wb-hjh933779 <wb-hjh933779@alibaba-inc.com>
2021-09-29 19:16:18 +0800

    fix null point error caused by using label plugin.

--- plugin/labels.go
+++ plugin/labels.go
@@ -68,7 +68,11 @@ func (l LabelsNodes) Run(context Context, phase Phase) error {
 	}
 	l.client = c
 	l.data = l.formatData(context.Plugin.Spec.Data)
-
+	k8sClient, err := k8s.Newk8sClient()
+	if err != nil {
+		return err
+	}
+	l.client = k8sClient
 	nodeList, err := l.client.ListNodes()
 	if err != nil {
 		return fmt.Errorf("current cluster nodes not found, %v", err)
